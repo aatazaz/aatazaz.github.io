cbd586a301aac6b519830e2a0507476db1480cd4
aatazaz <aatazaz.j27@gmail.com>
2021-03-27 15:04:46 +0500

    Update qr.php

--- qr.php
+++ qr.php
@@ -1,14 +1,21 @@
 <?php
   
 // Include the qrlib file
-include 'phpqrcode/qrlib.php';
+//include 'phpqrcode/qrlib.php';
   
 // $text variable has data for QR 
-$text = "GEEKS FOR GEEKS";
+//$text = "GEEKS FOR GEEKS";
   
 // QR Code generation using png()
 // When this function has only the
 // text parameter it directly
 // outputs QR in the browser
-QRcode::png($text);
+//QRcode::png($text);
+
+
+ 
+echo "<h2>PHP is Fun!</h2>";
+echo "Hello world!<br>";
+echo "I'm about to learn PHP!<br>";
+echo "This ", "string ", "was ", "made ", "with multiple parameters.";
 ?>
